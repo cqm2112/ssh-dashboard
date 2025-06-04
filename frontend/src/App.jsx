6cfda5a49e281d25a98d2b59927151e572f71dc7
cqm2112 <cqm2102gmail.com>
2025-06-04 16:40:38 -0400

    public

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 import './App.css';
 
-const socket = io('http://localhost:3001');
+const socket = io('http://ssh.sbmeek.com:3001');
 
 export default function App() {
   const [logs, setLogs] = useState('');
